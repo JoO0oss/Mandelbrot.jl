db9f5443c3b1e7ab496bb101fc066234a82aae35
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2023-05-12 12:32:41 +0100

    Latest iteration of the program.
    Not sure what's changed, it's been a while. Here's a version that does it into tiles that I've had a while but haven't yet uploaded.

--- src/mandelbrot_segments.jl
+++ src/mandelbrot_segments.jl
@@ -6,30 +6,53 @@ end
 import Images
 import Dates
 
+
+
 #### Numerical parameters, tweak as desired. ####
 
-PICTURE_UNIT = 600  # How many pixels per unit in the complex plane.
-MANDELBROT_MAX_ITERATIONS = 120
+PICTURE_UNIT = 10000  # How many pixels per unit in the complex plane.
+MANDELBROT_MAX_ITERATIONS = 180
 COLOUR_BAND_WIDTH = 30  # How many iterations to go to the next ("vibrant") colour.
+SEGMENT_SIZE = 500  # The width and height of each segment.
 
 UNIT_WIDTH = 3.2  # The range of real units wide the picture is.
 UNIT_HEIGHT = 2.5  # The range of complex units high the picture is.
+
+# Images which don't get whole numbers of segments have dimensions rounded up.
+
 
 # (The image starts 0 + 0i in the centre.)
 X_UNIT_OFFSET = 0.5  # Increase this number to move the image right.
 Y_UNIT_OFFSET = 0  # Increase this number to move the image up.
 
+# Filename options for the segments.
+SUBFOLDER_NAME = "segments"
+
 #### End of numerical parameters. ####
+
 
 
 REPORT_PROGRESS = true  # Percentage progress report.
 SILENCE_OUTPUT = false  # Hide away start and finish messages.
-OVERWRITE_EXISTING = false  # Automatically overwrite a picture without asking the user.
+ACCEPT_EXISTING = true  # True to skip any previously rendered segments.
 
-PICTURE_WIDTH = Int(round(PICTURE_UNIT * UNIT_WIDTH))
-PICTURE_HEIGHT = Int(round(PICTURE_UNIT * UNIT_HEIGHT))
+SEGMENT_NUM_X = Int(ceil(PICTURE_UNIT * UNIT_WIDTH / SEGMENT_SIZE))
+SEGMENT_NUM_Y = Int(ceil(PICTURE_UNIT * UNIT_HEIGHT / SEGMENT_SIZE))
+SEGMENT_NUM = SEGMENT_NUM_X * SEGMENT_NUM_Y
+
+MAX_STRING_PAD = length(string(SEGMENT_NUM))  # For padding the segment number in the filename.
+
 
 COLOUR_CYCLE_LENGTH = 5  # Five block colours cycle in increasing detail.
+
+function segment_name(x::Int, y::Int)::String
+    #return "$(SUBFOLDER_NAME)/mandelbrot_$(PICTURE_UNIT)_$(MANDELBROT_MAX_ITERATIONS)_$(x)_$(y).png"
+    num = x + (y-1) * SEGMENT_NUM_X
+    num_str = "0" ^ (MAX_STRING_PAD - length(string(num))) * string(num)
+    
+    return "$(SUBFOLDER_NAME)/mandelbrot_$(PICTURE_UNIT)_$(MANDELBROT_MAX_ITERATIONS)_$(num_str)_$(x)-$(y).png"
+end
+
 
 function mandelbrot(x::Float64, y::Float64)::Int
     c = x + y*im
@@ -100,78 +123,61 @@ end
 
 
 function generate()
-    filename = "mandelbrot_$(PICTURE_UNIT)_$(MANDELBROT_MAX_ITERATIONS).png"
+    img::Array{UInt8, 3} = fill(0, (SEGMENT_SIZE, SEGMENT_SIZE, 3))
 
-    commit_write::Bool = true
-    overwrite::Bool = false
-
-    if OVERWRITE_EXISTING
-        commit_write = true
-        overwrite = isfile(filename)
-    else
-        if isfile(filename)
+    if !isdir(SUBFOLDER_NAME)
+        try
+            mkdir(SUBFOLDER_NAME)
             if !SILENCE_OUTPUT
-                print("Picture already exists. Overwrite? [Y/n] ")
-                user_in = lowercase(readline())
-                overwrite = commit_write = (user_in == "y" || user_in == "yes" || user_in == "")
-            else
-                # If the user does not want output, then, if a file already exists, just stop.
-                commit_write = false
+                println("Folder '$SUBFOLDER_NAME' did not exist and has been created.")
             end
-        else
-            commit_write = true
-            overwrite = false
-        end
-    end
-
-    if !SILENCE_OUTPUT
-        if commit_write
-            if overwrite
-                println("Render target already exists: '$(filename)', overwriting.")
+        catch
+            if !SILENCE_OUTPUT
+                println("Error trying to write images into '$SUBFOLDER_NAME'.")
             end
-        else
-            println("Render target already exists: '$(filename)', skipping.")
         end
     end
-
-    if !commit_write
-        if !SILENCE_OUTPUT
-            println("File write cancelled. Exiting program early.")
-        end
-        return
-    end
-
-    img::Array{UInt8, 3} = fill(0, (PICTURE_HEIGHT, PICTURE_WIDTH, 3))
-
 
     if !SILENCE_OUTPUT
         println("Image processing started.")
     end
 
-    total_pixels = PICTURE_WIDTH * PICTURE_HEIGHT
-    pc_through = 0
-    pixels_through = 0
+    segment_num = 0
 
     start = Dates.now()
-    for x = 1:PICTURE_WIDTH
-        for y = 1:PICTURE_HEIGHT
-            cx, cy = pixel_to_complex(x, y)
-            mandel = mandelbrot(cx, cy)  # mandel stores the "depth"/number of iterations.
+    
+    for segment_x = 1:SEGMENT_NUM_X
+        for segment_y = 1:SEGMENT_NUM_Y
+            segment_num += 1
+
+            filename = segment_name(segment_x, segment_y)
+            if isfile(filename) && ACCEPT_EXISTING
+                if REPORT_PROGRESS
+                    print("Skipping '$filename'.  ($segment_num of $SEGMENT_NUM)")
+                end
+                continue
+            end
             
-            # Draw the colour accordingly, unless the point is in the mandelbrot set, then just leave that pixel.
-            if mandel != -1
-                mandel_colour = length_to_colour(mandel)
-                draw_point!(img, x, y, mandel_colour)
+            img = fill(0, (SEGMENT_SIZE, SEGMENT_SIZE, 3))
+
+            for x = 1:SEGMENT_SIZE
+                for y = 1:SEGMENT_SIZE
+                    cx, cy = pixel_to_complex((segment_x - 1) * SEGMENT_SIZE + x, (segment_y - 1) * SEGMENT_SIZE + y)
+                    mandel = mandelbrot(cx, cy)  # mandel stores the "depth"/number of iterations.
+
+                    # Draw the colour accordingly, unless the point is in the mandelbrot set, then just leave that pixel.
+                    if mandel != -1
+                        mandel_colour = length_to_colour(mandel)
+                        draw_point!(img, x, y, mandel_colour)
+                    end
+                end
+            end
+
+            Images.save(filename, img)
+            if REPORT_PROGRESS
+                println("Saved '$filename' ($segment_num of $SEGMENT_NUM).")
             end
         end
-        pixels_through += PICTURE_HEIGHT
-        if REPORT_PROGRESS && (pc_through != Int(round(pixels_through / total_pixels * 100)))
-            print(pc_through, "%  ")
-        end
-        pc_through = Int(round(pixels_through / total_pixels * 100))
-    end
-    if REPORT_PROGRESS
-        println("100%")
     end
     finish = Dates.now()
     
@@ -179,20 +185,17 @@ function generate()
         println("Image processing finished.")
 
         delta_seconds = round(Dates.value(finish - start) / 100) / 10
-        println("Mandelbrot set calculations took $delta_seconds seconds. (This does not include time to import libraries, open files etc.)")
+        println("Mandelbrot set calculations took $delta_seconds seconds. (This does not include time to import libraries etc.)")
     end
 
-    Images.save(filename, img)
 
-    if !SILENCE_OUTPUT
-        println("Image saved to '$filename'.")
-    end
+    # montage *.png -geometry +0+0 -tile 7x5 all.png
 end
 
 function main()
     global REPORT_PROGRESS
     global SILENCE_OUTPUT
-    global OVERWRITE_EXISTING
+    global ACCEPT_EXISTING
 
     if "-h" in ARGS || "--help" in ARGS
         println("Usage: mandelbrot.jl [OPTIONS]")
@@ -201,7 +204,7 @@ function main()
         println("Options:")
         println("  -h, --help       Print this help message.")
         println("  -s, --silence    Hide percentage progress report. If --silence=ALL, then silence all output.")
-        println("  -y, --overwrite  Overwrite existing image without asking.")
+        println("  -o, --overwrite  Overwrite existing images (default is to leave segments as they are).")
         return
     end
 
@@ -227,9 +230,9 @@ function main()
             if arg_name == "--silence" || arg_name == "-s"
                 # If just the silence option is provided, then give start and end outputs.s
                 REPORT_PROGRESS = false
-            elseif (arg_name == "--overwrite" || arg_name == "-y")
+            elseif (arg_name == "--overwrite" || arg_name == "-o")
                 # -y or "--overwrite" option means ignore existing image in that place.
-                OVERWRITE_EXISTING = true
+                ACCEPT_EXISTING = false
             else
                 println("Unrecognised argument: '$arg_name', use --help or -h for more information.")
                 return
@@ -242,4 +245,3 @@ end
 
 
 main()
-exit(0)
